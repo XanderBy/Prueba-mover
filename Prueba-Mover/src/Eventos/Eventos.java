d11b249e139b7e43015970e2dda2101490223131
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-04-23 19:54:06 +0200

    0.3
    E empezado los eventos de las teclas

--- Prueba-Mover/src/Eventos/Eventos.java
+++ Prueba-Mover/src/Eventos/Eventos.java
@@ -1,5 +1,35 @@
 package Eventos;
 
-public class Eventos {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import ClasesPojos.Personaje;
+import Metodos.Mover;
+
+public class Eventos implements KeyListener {
+	Personaje p=new Personaje(0, 0, 0);
+		@Override
+		public void keyPressed(KeyEvent e) {
+			adelanteKeyPressed(e);
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}  
+		
+	
+	private void adelanteKeyPressed(KeyEvent e) {                                   
+	       Mover.moverAdelante(p);
+	       
+	    }
 
 }
